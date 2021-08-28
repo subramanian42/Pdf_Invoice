c64d1fc0b1e745bed85e3e072ee2709f246c7e7b
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2021-08-29 02:10:56 +0530

    first commit

---
+++ lib/bussiness/pdf_invoice.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+
+//import 'package:pdf/pdf.dart';
+
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart';
+import 'package:testproject/bussiness/pdf_api.dart';
+
+import 'package:testproject/model/Weight.dart';
+
+class PdfInvoice {
+  static Future<File> generatePdf(List<Weight> weights, num totalWeight) async {
+    final pdf = Document();
+    pdf.addPage(MultiPage(
+        build: (context) => [
+              buildTitle(),
+              buildInvoice(weights),
+              Divider(),
+              buildTotal(weights),
+            ]));
+    return PdfApi.saveDocument(name: 'Weights_Receipt.pdf', pdf: pdf);
+  }
+
+  static Widget buildTitle() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Text(
+        'INVOICE',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 0.8 * PdfPageFormat.cm),
+    ]);
+  }
+
+  static Widget buildInvoice(List<Weight> weights) {
+    final headers = [
+      'Product Id',
+      'Product Weight',
+    ];
+    final List<List<String>> data = weights.map((weight) {
+      return [
+        '${weight.id}',
+        '${weight.value}',
+      ];
+    }).toList();
+    return Table.fromTextArray(
+        headers: headers,
+        data: data,
+        border: null,
+        headerStyle: TextStyle(fontWeight: FontWeight.bold),
+        headerDecoration: BoxDecoration(color: PdfColors.grey300),
+        cellHeight: 30,
+        cellAlignments: {
+          0: Alignment.center,
+          1: Alignment.center,
+        });
+  }
+
+  static Widget buildTotal(List<Weight> weights) {
+    final totalWeight = weights
+        .map((weight) => weight.value)
+        .reduce((value1, value2) => value1 + value2);
+
+    return Container(
+      alignment: Alignment.center,
+      child: Row(children: [
+        Spacer(flex: 5),
+        Expanded(
+            flex: 6,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Container(
+                child: Row(
+                  children: [
+                    Text('Total Weight',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    SizedBox(width: 0.8 * PdfPageFormat.cm),
+                    Text('$totalWeight',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+              SizedBox(height: 2 * PdfPageFormat.mm),
+              Container(height: 1, color: PdfColors.grey400),
+              SizedBox(height: 0.5 * PdfPageFormat.mm),
+              Container(height: 1, color: PdfColors.grey400),
+            ]))
+      ]),
+    );
+  }
+}
---
+++ lib/screen/shopscreen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:testproject/bussiness/weight_database.dart';
+import 'package:testproject/model/Weight.dart';
+import 'package:testproject/screen/receipt_screen.dart';
+
+class ShopScreen extends StatefulWidget {
+  ShopScreen({Key? key}) : super(key: key);
+
+  @override
+  _ShopScreenState createState() => _ShopScreenState();
+}
+
+class _ShopScreenState extends State<ShopScreen> {
+  int sum = 0;
+  final TextEditingController weightcontroller = TextEditingController();
+  bool _validate = false;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Color getColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+      MaterialState.hovered,
+      MaterialState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Color(0XFFFF8474);
+    }
+    return Color(0XFFC1CFC0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0XFFE7E0C9),
+        body: Padding(
+          padding: EdgeInsets.only(left: 25, right: 25, top: 150),
+          child: Column(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                child: Image.asset("assets/weight.png"),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Weight',
+                  hintText: 'Enter Weight',
+                  border: OutlineInputBorder(),
+                  errorText:
+                      _validate ? 'Weight value should not be null' : null,
+                  suffixIcon: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0, vertical: 2.0),
+                    child: ElevatedButton(
+                      child: Text('ADD'),
+                      onPressed: () async {
+                        setState(() {
+                          weightcontroller.text.isEmpty
+                              ? _validate = true
+                              : _validate = false;
+                        });
+                        if (weightcontroller.text.isNotEmpty) {
+                          Weight newWeight = Weight(
+                            value: num.parse(weightcontroller.text),
+                          );
+                          await WeightDatabase.instance.create(newWeight);
+                          setState(() {
+                            weightcontroller.text = '';
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                controller: weightcontroller,
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.resolveWith(getColor)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return ReceiptScreen();
+                        },
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'GENERATE RECIPT',
+                    style: TextStyle(color: Colors.black),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
